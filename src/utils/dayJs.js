1a5cba952b680a3da8a1848d68f2027353a9ae20
wb135 <wb135@yingu.com>
2019-09-05 22:25:50 +0800

    28.在utils中,进行dayjs的汉化

--- src/utils/dayJs.js
+++ src/utils/dayJs.js
@@ -1,13 +1,15 @@
 /*
- * @Description: 相对时间的过滤器  from xx时间后   to xxx时间前
+ * @Description: 相对时间的过滤器  from xx时间后   to xxx时间前   汉化
  * @Author: your name
  * @Date: 2019-09-05 22:09:48
- * @LastEditTime: 2019-09-05 22:15:24
+ * @LastEditTime: 2019-09-05 22:24:36
  * @LastEditors: Please set LastEditors
  */
 
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
 export const fmDate = (value) => {
   return dayjs().to(dayjs(value))
