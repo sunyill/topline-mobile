1c0e5edd737ed85f5fa74a76cc2c1f86eff0765c
wb135 <wb135@yingu.com>
2019-09-07 13:08:12 +0800

    41.频道管理的界面

--- src/views/home/components/channelEdit.vue
+++ src/views/home/components/channelEdit.vue
@@ -2,7 +2,7 @@
  * @Description: 负责tab, 点击图标,显示弹出层
  * @Author: your name
  * @Date: 2019-09-07 11:15:21
- * @LastEditTime: 2019-09-07 12:42:08
+ * @LastEditTime: 2019-09-07 13:06:47
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -12,7 +12,27 @@
     position="bottom"
     round
     :style="{ height: '80%' }"
-  >hello</van-popup>
+  >
+    <van-cell icon="cross" @click="$emit('input',false)" />
+    <!-- 我的频道界面 -->
+    <van-cell title="我的频道" label="点击进入我的频道">
+      <van-button round type="danger" size="mini">编辑</van-button>
+    </van-cell>
+    <van-grid>
+      <van-grid-item v-for="value in 8" :key="value" title="标题" text="文字">
+        <van-icon slot="icon" class="close-icon" name="close">
+
+        </van-icon>
+      </van-grid-item>
+    </van-grid>
+    <!-- 推荐列表 -->
+<van-cell title="推荐列表" label="点击添加频道">
+
+</van-cell>
+<van-grid>
+        <van-grid-item v-for="value in 7" :key="value" text='文字'></van-grid-item>
+    </van-grid>
+  </van-popup>
 </template>
 
 <script>
@@ -25,12 +45,15 @@ export default {
     }
   },
   data () {
-    return {
-
-    }
+    return {}
   }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.close-icon {
+    position: absolute;
+    top: 0;
+    right: 0;
+}
 </style>
