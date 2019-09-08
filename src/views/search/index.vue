00f74d337fa38a55dd72713898264f1834f898b6
wb135 <wb135@yingu.com>
2019-09-08 19:24:39 +0800

    54.将搜索记录存储到本地

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -2,7 +2,7 @@
  * @Description: search页面
  * @Author: your name
  * @Date: 2019-09-07 21:34:09
- * @LastEditTime: 2019-09-07 22:40:35
+ * @LastEditTime: 2019-09-08 19:21:37
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -11,7 +11,7 @@
       v-model="value"
       placeholder="请输入搜索关键词"
       show-action
-      @search="onSearch"
+      @search="onSearch(value)"
       @cancel="onCancel"
       @input="handleInput"
       clearable
@@ -19,40 +19,65 @@
     />
     <!-- 搜索提示 -->
     <van-cell-group v-show="value">
-      <van-cell v-for="item in suggestList" :key="item" :title="item" icon="search" />
+      <van-cell v-for="item in suggestList" :key="item" @click="onSearch(item)" :title="item" icon="search" />
 
     </van-cell-group>
     <!-- 历史记录 -->
     <van-cell-group v-show="!value">
       <van-cell title="历史记录">
         <!-- 右侧显示内容 -->
-        <div>
+        <div v-show="isEdit" style="display:inline-block">
           <span>全部删除</span>&nbsp;
-          <span>完成</span>&nbsp;
-          <van-icon name="delete" size="16px"></van-icon>
+          <span @click="isEdit=false">完成</span>&nbsp;
         </div>
+          <van-icon v-show="!isEdit" name="delete" size="16px" @click="isEdit=true"></van-icon>
       </van-cell>
-      <van-cell title="花海" icon="hot-o">
+      <van-cell  icon="hot-o" v-for="(item,index) in histories" :key="index" :title="item">
         <!-- 右侧显示内容 -->
-        <van-icon name="close" size="18px" />
+        <van-icon name="close" size="18px" v-show="isEdit" @click="handleDelete(index)" />
       </van-cell>
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import * as localStorage from '../../utils/localStorage'
 import { getSuggestion } from '@/api/search'
+import { mapState } from 'vuex'
 export default {
   data () {
     return {
       value: '',
+      // 编辑模式
+      isEdit: false,
       // 存放搜索建议的数组
-      suggestList: []
+      suggestList: [],
+      // 历史记录的数组
+      histories: []
     }
   },
+  computed: {
+    ...mapState(['user'])
+  },
   methods: {
-    onSearch () {},
+    onSearch (item) {
+      if (this.histories.includes(item)) {
+
+      }
+      // 记录搜索历史
+      this.histories.push(item)
+      // 判断用户是否登录
+      if (this.user) {
+
+      }
+      // 没有登录
+      localStorage.setItem('history', this.histories)
+    },
     onCancel () {},
+    handleDelete (index) {
+      this.histories.splice(index, 1)
+      localStorage.setItem('history', this.histories)
+    },
     /**
    * @description: 搜索input 获取焦点的时刻触发的时间
    * @param {type}
@@ -66,6 +91,14 @@ export default {
         console.log(error)
       }
     }
+  },
+  created () {
+    if (this.user) {
+      // 如果用户存在,服务器获取数据
+      return
+    }
+    // 没登录,本地获取,  第一次获取的话,可能是空, 返回为null
+    this.histories = localStorage.getItem('history') || []
   }
 }
 </script>
