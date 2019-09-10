22f65a24e35af5b84c81127029b46ba931aee2e7
wb135 <wb135@yingu.com>
2019-09-10 15:00:35 +0800

    76.评论api

---
+++ src/api/comment.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 评论api
+ * @Author: your name
+ * @Date: 2019-09-10 14:41:17
+ * @LastEditTime: 2019-09-10 14:59:47
+ * @LastEditors: Please set LastEditors
+ */
+import request from '@/utils/request'
+
+export const getComments = ({
+  isArticle,
+  source,
+  offset,
+  limit
+}) => {
+  return request.get('/app/v1_0/comments', {
+    // a, 对文章的评论, c 对评论的评论
+    type: isArticle ? 'a' : 'c',
+    source,
+    offset,
+    limit
+  })
+}
