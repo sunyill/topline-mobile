3689e6180133e91e9020c91a9d65e474407c2660
wb135 <wb135@yingu.com>
2019-09-12 13:04:22 +0800

    84.我的页面,根据登录未登录,显示不同的界面

--- src/views/TabBar.vue
+++ src/views/TabBar.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-05 19:58:52
- * @LastEditTime: 2019-09-12 12:33:35
+ * @LastEditTime: 2019-09-12 12:52:58
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -12,17 +12,23 @@
       <van-tabbar-item name="home" icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item name="search" icon="search" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item name="friends" icon="friends-o" to="/video">视频</van-tabbar-item>
-      <van-tabbar-item name="setting" icon="setting-o" to="/user">我的</van-tabbar-item>
+      <!-- 根据是否登录,显示页面 -->
+      <van-tabbar-item name="setting" icon="setting-o" to="/user">{{user?"我的":"未登录"}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
+  name: 'tabBar',
   data () {
     return {
       active: 'home'
     }
+  },
+  computed: {
+    ...mapState(['user'])
   }
 }
 </script>
--- src/views/home/User.vue
+++ src/views/home/User.vue
@@ -2,19 +2,53 @@
  * @Description: 我的界面
  * @Author: your name
  * @Date: 2019-09-12 12:23:25
- * @LastEditTime: 2019-09-12 12:23:50
+ * @LastEditTime: 2019-09-12 13:02:43
  * @LastEditors: Please set LastEditors
  -->
 <template>
-    <div>我的</div>
+    <div class="not-login">
+        <div class="circle" @click="handleLogin">
+            <span>登录</span>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+  name: 'user',
+  methods: {
+    handleLogin () {
+      this.$router.push({
+        path: '/login',
+        query: {
+          redirect: this.$route.fullPath
+        }
+      })
+    }
+  }
 
 }
 </script>
 
-<style>
-
+<style lang="less">
+.not-login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url("../../assets/img/bg_blue_car.png");
+  background-size: cover;
+  .circle {
+    width: 50px;
+    height: 50px;
+    padding: 20px;
+    background-color: red;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  }
+}
 </style>
