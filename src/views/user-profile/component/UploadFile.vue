c2b16191e7804a176be90b9ad4794e807594764b
wb135 <wb135@yingu.com>
2019-09-12 15:02:59 +0800

    90.头像-换头像框,头像预览

--- src/views/user-profile/component/UploadFile.vue
+++ src/views/user-profile/component/UploadFile.vue
@@ -2,7 +2,7 @@
  @Description: 上传图片,弹出对话框
  * @Author: your name
  * @Date: 2019-09-12 14:33:08
- * @LastEditTime: 2019-09-12 14:35:38
+ * @LastEditTime: 2019-09-12 15:02:08
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -13,7 +13,8 @@
     closeOnClickOverlay
   >
     <van-cell-group>
-      <van-cell title="从相册选择图片" />
+      <van-cell title="从相册选择图片"  @click="handleSelectFile"/>
+      <input ref="file" type="file" style="display: none;">
       <van-cell title="拍照" />
       <van-cell title="取消" @click="$emit('input', false)" />
     </van-cell-group>
@@ -21,10 +22,39 @@
 </template>
 
 <script>
-
+import Vue from 'vue'
+import { ImagePreview } from 'vant'
+// 引用图片预览
+Vue.use(ImagePreview)
 export default {
   name: 'UploadFile',
-  props: ['value']
+  props: ['value'],
+  methods: {
+    handleSelectFile () {
+      this.$refs.file.click()
+      // 给file改变注册事件
+      this.$refs.file.onchange = (e) => {
+        if (e.target.files.length === 0) {
+          return
+        }
+        // 处理
+        // const url = URL.createObjectURL(e.target.files[0])
+        const url = URL.createObjectURL(e.target.files[0])
+        // 关闭对话框
+        this.$emit('input', false)
+        // 图片预览
+        ImagePreview({
+          images: [url],
+          showIndex: false,
+          onClose () {
+            // 操作关闭
+          }
+        }
+
+        )
+      }
+    }
+  }
 }
 </script>
 
