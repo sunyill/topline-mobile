6edf113a779ea4a63f1e3a7558018a2fac37af01
wb135 <wb135@yingu.com>
2019-09-12 13:57:21 +0800

    我的页面,登录页面

--- src/views/TabBar.vue
+++ src/views/TabBar.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-09-05 19:58:52
- * @LastEditTime: 2019-09-12 12:52:58
+ * @LastEditTime: 2019-09-12 13:54:58
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -13,7 +13,7 @@
       <van-tabbar-item name="search" icon="search" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item name="friends" icon="friends-o" to="/video">视频</van-tabbar-item>
       <!-- 根据是否登录,显示页面 -->
-      <van-tabbar-item name="setting" icon="setting-o" to="/user">{{user?"我的":"未登录"}}</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o" to="/user">{{user?'我的':'未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
--- src/views/home/User.vue
+++ src/views/home/User.vue
@@ -2,20 +2,71 @@
  * @Description: 我的界面
  * @Author: your name
  * @Date: 2019-09-12 12:23:25
- * @LastEditTime: 2019-09-12 13:02:43
+ * @LastEditTime: 2019-09-12 13:56:46
  * @LastEditors: Please set LastEditors
  -->
 <template>
-    <div class="not-login">
-        <div class="circle" @click="handleLogin">
-            <span>登录</span>
-        </div>
+  <!-- // 未登录 -->
+  <div>
+    <div v-if="!user" class="not-login">
+      <div class="circle" @click="handleLogin">
+        <span>登录</span>
+      </div>
     </div>
+    <!-- 显示登录 -->
+    <div v-else>
+      <van-cell-group class="user-info">
+        <van-cell class="base-info" is-link :border="false">
+          <div slot="title">
+            <img class="avatar" src="http://toutiao.meiduo.site/FgSTA3msGyxp5-Oufnm5c0kjVgW7" alt />
+            <span class="title">只是为了好玩儿</span>
+          </div>
+        </van-cell>
+        <van-grid class="data-info" :border="false">
+          <van-grid-item>
+            <span class="count">1</span>
+            <span class="text">头条</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">2</span>
+            <span class="text">关注</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">3</span>
+            <span class="text">粉丝</span>
+          </van-grid-item>
+          <van-grid-item>
+            <span class="count">4</span>
+            <span class="text">获赞</span>
+          </van-grid-item>
+        </van-grid>
+      </van-cell-group>
+      <van-cell-group>
+        <van-grid clickable>
+          <van-grid-item icon="star" text="我的收藏" />
+          <van-grid-item icon="chat" text="我的评论" />
+          <van-grid-item icon="like" text="我的点赞" />
+          <van-grid-item icon="browsing-history" text="浏览历史" />
+        </van-grid>
+      </van-cell-group>
+      <van-cell-group>
+        <van-cell title="消息通知" is-link />
+        <van-cell title="用户反馈" is-link />
+        <van-cell title="小智同学" is-link />
+        <van-cell title="系统设置" is-link to="/settings" />
+      </van-cell-group>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-  name: 'user',
+  name: 'User',
+  computed: {
+    ...mapState(['user'])
+  },
+
   methods: {
     handleLogin () {
       this.$router.push({
@@ -26,7 +77,6 @@ export default {
       })
     }
   }
-
 }
 </script>
 
@@ -49,6 +99,36 @@ export default {
     align-items: center;
     color: #fff;
     font-size: 20px;
+  }
+}
+.user-info {
+  .base-info {
+    display: flex;
+    align-items: center;
+    background-color: #0096fa;
+    div {
+      display: flex;
+      align-items: center;
+    }
+    .avatar {
+      margin-right: 15px;
+      width: 50px;
+      border-radius: 100%;
+    }
+  }
+  .data-info {
+    .text {
+      font-size: 14px;
+    }
+    .count {
+      font-size: 12px;
+    }
+  }
+  /deep/ .van-cell__right-icon {
+    color: #fff;
+  }
+  /deep/ .van-grid-item__content {
+    background-color: #0096fa;
   }
 }
 </style>
