96aa92c4e846ea5e36592d60a904833b87751a57
wb135 <wb135@yingu.com>
2019-09-12 16:12:58 +0800

    94.切换关注/粉丝,在activeted钩子函数处理

--- src/views/Follow.vue
+++ src/views/Follow.vue
@@ -2,7 +2,7 @@
  * @Description:我的 - 关注
  * @Author: your name
  * @Date: 2019-09-12 15:35:21
- * @LastEditTime: 2019-09-12 15:37:02
+ * @LastEditTime: 2019-09-12 16:11:16
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -10,7 +10,7 @@
     <!-- 导航头 -->
     <van-nav-bar title="关注/粉丝" left-arrow @click-left="$router.back()" />
     <!-- tabs -->
-    <van-tabs v-model="active">
+    <van-tabs v-model="active" color="#57bd6a">
       <van-tab title="关注">内容 1</van-tab>
       <van-tab title="粉丝">内容 2</van-tab>
     </van-tabs>
@@ -24,6 +24,17 @@ export default {
   data () {
     return {
       active: 1
+    }
+  },
+  created () {
+
+  },
+  activated () {
+    const type = this.$route.query.type
+    if (type === '1') {
+      this.active = 0
+    } else {
+      this.active = 1
     }
   }
 }
